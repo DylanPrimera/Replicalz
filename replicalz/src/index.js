04bdf5a8abfa748d701b8774f0b88dda568fceaa
DylanPrimera <dylanprimera98@gmail.com>
2020-05-31 18:24:43 -0500

    navbar

---
+++ replicalz/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Principal from './principal/principal'
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+ReactDOM.render(
+  <Principal />,
+  document.getElementById('root')
+);
---
+++ replicalz/src/principal/principal.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Nab from '../components/nab'
+class principal extends React.Component{
+    render(){
+        return( <div >
+            <div><Nab /></div>
+            </div>);   
+        
+    }
+}
+export default principal;
